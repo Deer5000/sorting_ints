50b85d43070de79d42df66777291695a840e080e
Deer5000 <khidrbrinkley32@gmail.com>
2021-02-25 17:43:08 -0500

    bucket_sort complete

--- sorting_integer.py
+++ sorting_integer.py
@@ -38,7 +38,46 @@ print("sorted array:  " + str(answer))
 #     # FIXME: Improve this to mutate input instead of creating new output list
 
 
-# def bucket_sort(numbers, num_buckets=10):
+def bucket_sort(numbers, num_buckets=10):
+    max_int = max(numbers)
+    min_int = min(numbers)
+
+    range_of_nums = (max_int - min_int) / num_buckets
+
+    temporary_list = []
+
+    for i in range(num_buckets):
+        temporary_list.append([])
+
+    for i in range(len(numbers)):
+        bucket_place = (numbers[i] - min_int) / range_of_nums - int((numbers[i] - min_int) / range_of_nums)
+
+        if (bucket_place == 0 and numbers[i] != min_int):
+            temporary_list[int((numbers[i] - min_int) / range_of_nums) - 1].append(numbers[i])
+        else:
+            temporary_list[int((numbers[i] - min_int) / range_of_nums)].append(numbers[i])
+
+    for i in range(len(temporary_list)):
+        if len(temporary_list[i]) != 0:
+            temporary_list[i].sort()
+
+    n = 0
+
+    for j in temporary_list:
+        if j:
+            for i in j:
+                numbers[n] = i
+                n = n+1
+
+arr = [1, 0, 23, 273, 1, 23, 0, 13, 3, 0.123]
+bucks = 10
+bucket_sort(arr, bucks)
+print("Sorted array: ", arr)
+
+
+
+
+
 #     """Sort given numbers by distributing into buckets representing subranges,
 #     then sorting each bucket and concatenating all buckets in sorted order.
 #     TODO: Running time: ??? Why and under what conditions?
